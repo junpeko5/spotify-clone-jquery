7db725a4081da9a3dd4c2701af101c1db8259bfb
junpeko5 <junpeko5@gmail.com>
2019-01-28 04:28:32 +0900

    ログイン、登録ページのスタイルを設定

---
+++ asset/js/register.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#hideLogin').on('click', function() {
+    $('#loginForm').hide()
+    $('#registerForm').show()
+  })
+
+  $('#hideRegister').on('click', function() {
+    $('#loginForm').show()
+    $('#registerForm').hide()
+  })
+});
